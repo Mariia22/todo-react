8ffef55a7f1d491f08314be11823844ba3424bd4
Maria22 <m.ushakova22@gmail.com>
2021-11-28 17:45:08 +0100

    add normalize css

---
+++ src/types/theme.ts
@@ -0,0 +1,16 @@
+export enum Colors {
+  VeryLightBlueGrey = 'hsl(236, 33%, 92%)',
+  LightGrey = 'hsl(0, 0%, 98%)',
+  VeryDarkGreyBlue = 'hsl(235, 19%, 35%)',
+  FontColor = 'black'
+}
+
+export enum Backgrounds {
+  BackgroundMobileHeaderLight = 'url("../../images/bg-mobile-light.jpg") no - repeat hsl(0, 0%, 98%)',
+  BackgroundMobileSwitchMoon = 'url("../../images/icon-moon.svg") no - repeat transparent center',
+  BackgroundDesktopLight = 'url("../../images/bg-desktop-light.jpg")',
+  BackgroundMobileHeaderDark = 'url("../../images/bg-mobile-dark.jpg") no-repeat hsl(0, 0%, 98%)',
+  BackgroundMobileSwitchSun = 'url("../../images/icon-sun.svg") no-repeat transparent center',
+  BackgroundDesktopDark = 'url("../../images/bg-desktop-dark.jpg") no-repeat hsl(0, 0%, 98%)'
+}
+
