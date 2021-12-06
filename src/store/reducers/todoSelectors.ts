9a189c93151d5e9d0a5f87a4962b570ae0d811c8
Maria22 <m.ushakova22@gmail.com>
2021-12-06 20:28:31 +0100

    add selector to filter

--- src/store/reducers/todoSelectors.ts
+++ src/store/reducers/todoSelectors.ts
@@ -1,14 +1,13 @@
 import { TodoType } from './../../types/todo';
-import { FilterConst } from "../../types/todo";
 
-/*export const getTodoByFilter = (array: Array<TodoType>, filterParam: string): Array<TodoType> => {
+export const getTodoByFilter = (todos: Array<TodoType>, filterParam: string): Array<TodoType> => {
   switch (filterParam) {
     case 'ALL':
-      return array
+      return todos
     case 'COMPLETED':
-      return array.filter((todo: any) => todo.checked)
+      return todos.filter((todo: any) => todo.checked)
     case 'ACTIVE':
-      return array.filter((todo: any) => !todo.checked)
-    default: return array
+      return todos.filter((todo: any) => !todo.checked)
+    default: return todos
   }
-}*/
+}
